4660c995b3b857968a467a1855500456f20c6a03
Kamayani-R <kamayanir12@gmail.com>
2019-10-16 15:07:42 +0530

    Create new single page view using storyboard

---
+++ WarCardGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WarCardGame
+//
+//  Created by Kamayani on 16/10/19.
+//  Copyright © 2019 Kamayani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
